4f213d082ab8d8e01417380224efd034d32c4a3d
UsmanDevStuff <entpnr.usman@gmail.com>
2023-08-16 16:06:39 +0500

    track-order page update : huge

--- app/(routes)/track-order/components/InputForm.tsx
+++ app/(routes)/track-order/components/InputForm.tsx
@@ -1,36 +1,51 @@
 "use client";
 import React from "react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const InputForm = async (data: any) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const orders = data.data;
   const orderId = "ed3f750f-4c8f-4fbe-9525-fa51d35edd3c";
-  const [state, setState] = useState("");
+  const [value, setValue] = useState("");
+  let orderstatus;
   function handleClick(e: any) {
     e.preventDefault();
+    const inputId = inputRef.current?.value;
+
     function getorderid(order: any) {
-      return order.orderId === state;
+      return order.orderId === inputId;
     }
 
-    var gettask = orders.find(getorderid);
-    var orderid = JSON.stringify(gettask);
+    var orderid = orders.find(getorderid);
+    const orderstatus = orderid.orderStatus;
+    setValue(orderstatus);
 
-    if (orderid != undefined) console.log(orderid);
+    if (orderid != undefined) console.log(orderid.orderStatus);
     else console.log(`cannot find the given orderid`);
-    console.log(orders);
 
-    //console.log(state);
+    console.log(`your input is : ${inputId}`);
+    return orderstatus;
   }
   return (
-    <div>
-      <form>
+    <div className="border border-solid border-black rounded-md flex justify-center">
+      <form className="flex flex-col w-1/2">
         <label htmlFor="">Enter Your Order Id</label>
         <input
+          className="border border-solid border-gray-600"
+          ref={inputRef}
           name="state"
           type="text"
-          onChange={(e) => setState(e.target.value)}
+          //onChange={(e) => setState(e.target.value)}
         />
-        <button onClick={handleClick}>Track</button>
+        <button
+          className="m-4 border border-solid border-black bg-black text-white w-16"
+          onClick={handleClick}
+        >
+          Track
+        </button>
+        <div>
+          <h1>Order Status : {value}</h1>
+        </div>
       </form>
     </div>
   );
