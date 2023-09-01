8f9a4567564cf1fe3d6686cffb1f32f35f3d65da
UsmanDevStuff <entpnr.usman@gmail.com>
2023-09-01 20:38:52 +0500

    navbar update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,20 +14,20 @@ const Navbar = async () => {
         <Container>
           <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
             <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-              <p className="font-bold text-2xl">ShopTegic</p>
+              <p className="font-bold text-2xl">BeautyShop</p>
             </Link>
             {/* <MainNav data={categories} /> */}
             <NavbarActions />
           </div>
         </Container>
       </div>
-      <div className="border-b">
+      {/* <div className="border-b">
         <Container>
           <div className="py-5 flex">
             <MainNav data={categories} />
           </div>
         </Container>
-      </div>
+      </div> */}
     </>
   );
 };
