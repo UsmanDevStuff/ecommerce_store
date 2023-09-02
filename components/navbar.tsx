658c79ad258c43427c61dd88a5299d9155259c79
UsmanDevStuff <entpnr.usman@gmail.com>
2023-09-02 18:46:02 +0500

    ui fixes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = async () => {
         <Container>
           <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
             <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-              <p className="font-bold text-2xl">BeautyShop</p>
+              <p className="font-bold text-xl">BeautyShop</p>
             </Link>
             {/* <MainNav data={categories} /> */}
             <NavbarActions />
