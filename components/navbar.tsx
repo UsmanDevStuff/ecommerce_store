dbfc915dcc24e4374c499a76aea95973c36ac5b2
UsmanDevStuff <entpnr.usman@gmail.com>
2023-09-23 19:53:36 +0500

    logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,7 +15,8 @@ const Navbar = async () => {
         <Container>
           <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
             <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-              <p className="font-extrabold text-2xl">EllisMart</p>
+              <Image src={"/em-l-c.png"} alt="logo" height={49} width={95} />
+              {/* <p className="font-extrabold text-2xl">EllisMart</p> */}
             </Link>
             {/* <MainNav data={categories} /> */}
             <NavbarActions />
